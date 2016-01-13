624348d944bec0798062c9a32a94d9a7fad3b9c9
Blake Scott and Chris Young <blakerscott@gmail.com>
2016-01-12 16:05:57 -0800

    add theme toggle and hide button

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,14 @@
 $(document).ready(function() {
   $("button#changebg").click(function() {
     $("body").addClass("dark-theme");
+    $("button#changebg").hide();
+    $("button#resettheme").show();
+
   });
   $("button#resettheme").click(function() {
     $("body").removeClass();
+    $("button#changebg").show();
+    $("button#resettheme").hide();
+
   });
 })
